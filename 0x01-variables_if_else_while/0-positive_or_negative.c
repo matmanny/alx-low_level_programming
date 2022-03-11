23b41dab46046c30b46493934399bd69137e30cc
matmanny <mannyfekade@gmail.com>
2022-03-12 00:01:43 +0300

    pos and neg

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,10 +3,11 @@
 #include <time.h>
 /* more headers goes there */
 /* betty style doc for function main goes there */
-/* 
+/*
 *
-*main: function
+*main function
 */
+/* main function*/
 int main(void)
 {
 int n;
@@ -14,10 +15,10 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 /* your code goes there */
 if (n > 0)
-printf(" %d is positive",n);
+printf(" %d is positive", n);
 else if (n < 0)
-printf(" %d is negative",n);
+printf(" %d is negative", n);
 else
-printf(" %d is zero",n);
+printf(" %d is zero", n);
 return (0);
 }

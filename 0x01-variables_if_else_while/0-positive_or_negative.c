537400e535dde80e9c012b7ef910560f563e4b45
matmanny <mannyfekade@gmail.com>
2022-03-12 09:50:36 +0300

    pos and neg

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,9 +4,9 @@
 /* more headers goes there */
 /* betty style doc for function main goes there */
 /**
-*main -Entry point
+*main - Entry point
 *
-Return: Always 0 (Success/correct)
+*Return: Always 0 (Success/correct)
 */
 int main(void)
 {

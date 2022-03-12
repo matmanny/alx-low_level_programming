7e416ac275e04d230674701f368922bc13f50337
matmanny <mannyfekade@gmail.com>
2022-03-12 16:36:40 +0300

    print alpha

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,11 +10,12 @@ char lower_case;
 char upper_case;
 for (upper_case = 'A'; upper_case <= 'Z'; upper_case++)
 {
-	putchar(upper_case)
+	putchar(upper_case);
 }
 for (lower_case = 'a'; lowercase <= 'z'; lower_case++)
 {
-	putchar(lower_case)
+	putchar(lower_case);
 }
+putchar('\n');
 return (0);
 }

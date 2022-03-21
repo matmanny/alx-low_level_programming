5a06e79b07aeafa13bd5d939d7fef4c7c96cba60
matmanny <mannyfekade@gmail.com>
2022-03-21 16:25:36 +0300

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int -swapping 2 integer using a pointer
+ * @a:integer
+ * @b"integer
+ * Retuen:void
+ */
+void swap_int(int *a, int *b)
+{
+	int x;
+	x = *a;
+	*a = *b;
+	*b = x;
+	return;
+}
+	

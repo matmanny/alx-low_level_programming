67527d71ef4dbf9c42bb03a5bc8853c057c4e28b
matmanny <mannyfekade@gmail.com>
2022-03-21 17:00:29 +0300

    strlin

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * _strlin - length of string
+ * _strlen - length of string
  * @s:char
  * Return:int
  */
 int _strlen(char *s)
 {
 int i;
-for (i =0; s[i] != '\0'; i++)
+for (i = 0; s[i] != '\0'; i++)
 {
 continue;
 }

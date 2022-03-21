fc6cf741e3103e4da10d9f05f461eb8772cd19ab
matmanny <mannyfekade@gmail.com>
2022-03-21 16:50:32 +0300

    strlin

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlin - length of string
+ * @s:char
+ * Return:int
+ */
+int_strlen(char *s)
+{
+	int i;
+	for (i =0; s[i] != '0'; i++)
+	{
+		continue;
+	}
+	return (i);
+}
+

d8e3a364e72292d095a0182d47c8d9ea17838002
matmanny <mannyfekade@gmail.com>
2022-03-22 10:53:47 +0300

    jeygen

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,14 +1,24 @@
-#include "main.h"
+#include "stdio.h"
 #include <stdlib.h>
 #include <time.h>
-#include "stdio.h"
 /**
+ * main - generates keygen.
+ * Return: 0 Always.
  */
 int main(void)
 {
-	int number;
-	srand(time(0));
-	number = rand();
-	printf("%i\n", number);
+	int r = 0, c = 0;
+	time_t t;
+
+	srand((unsigned int) time(&t));
+	while (c < 2772)
+	{
+		r = rand() % 128;
+		if ((c + r) > 2772)
+			break;
+		c = c + r;
+		printf("%c", r);
+	}
+	printf("%c\n", (2772 - c));
 	return (0);
 }

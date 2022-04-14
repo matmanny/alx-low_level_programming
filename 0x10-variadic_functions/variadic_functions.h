ca88e280bcd15efc0ff02de564d530fce8524b96
matmanny <mannyfekade@gmail.com>
2022-04-14 22:20:17 +0300

    variadic

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,28 +1,12 @@
-#include "variadic_functions.h"
-/**
- * sum_them_all - sum of variadic variable
- * @n:const
- * Return:int
- */
-int sum_them_all(const unsigned int n, ...)
-{
-unsigned int i;
-va_list list;
-double sum = 0;
+#ifndef _VARIADIC_FUNCTIONS_
+#define _VARIADIC_FUNCTIONS_
 
-	if (n == 0)
-	{
-		return (0);
-	}
-	if (n == 0)
-	{
-		return (0);
-	}
-va_start(list, n);
-	for (i = 0; i < n; i++)
-	{
-		sum += va_arg(list, int);
-	}
-	va_end(list);
-	return (sum);
-}
+#include <stdarg.h>
+#include <stdio.h>
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif

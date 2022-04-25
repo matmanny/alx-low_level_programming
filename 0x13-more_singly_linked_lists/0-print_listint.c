1644a64623b783bd4fb60dcc7071335ae5a74f8c
matmanny <mannyfekade@gmail.com>
2022-04-25 21:10:10 +0300

    more singly

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * print_listint - print all element in a list
+ * @h: pointer in a list
+ * Return:size of the list
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t x = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		x++;
+		h = h->next;
+	}
+	return (x);
+}
